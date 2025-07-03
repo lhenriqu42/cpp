8324cbc108a55e921feddf1fe15534fefa1c37df
lhenriqu42 <luizhapsouza@gmail.com>
2025-07-03 19:07:14 -0300

    Module 02 Initialized

---
+++ Module-02/ex00/includes/Fixed.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 08:41:54 by lhenriqu          #+#    #+#             */
+/*   Updated: 2025/07/03 18:59:12 by lhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+
+class Fixed {
+	public:
+		Fixed();
+		Fixed(Fixed const &src);
+		~Fixed();
+
+		Fixed&	operator=(Fixed const &rSym);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+	private:
+		int					value;
+		static const int	bits = 8;
+};
+
+#endif
---
+++ Module-02/ex00/src/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 09:09:37 by lhenriqu          #+#    #+#             */
+/*   Updated: 2025/07/03 19:03:48 by lhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
